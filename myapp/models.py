376bbfcca14e1869046d72f34ae47d3bb5dcb0b5
nddakshinamoorthy07 <nddakshinamoorthy07@gmail.com>
2025-03-12 11:55:07 +0530

    com

---
+++ myapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
